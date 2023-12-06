d6eeabf49bd3b43aef4948c4cea3eabf16f53cfe
Victor Bernardes <victor_luizbernardes5@outlook.com>
2023-12-06 17:26:26 -0300

    day3 xmas o CARALEO

--- day3/main.go
+++ day3/main.go
@@ -4,13 +4,17 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strconv"
 	"strings"
+	"unicode"
 )
+
+const MATRIZ_SIZE = 140
 
 func read(filepath string) [][]string {
 	file, err := os.ReadFile(filepath)
 
-	matriz := make([][]string, 140)
+	matriz := make([][]string, MATRIZ_SIZE)
 
 	if err != nil {
 		log.Fatal(err)
@@ -51,23 +55,129 @@ func generateOffsets() []PosOffSet {
 	return offsets
 }
 
-func getNeighboursPos(x, y int, matriz *[][]string) []int {
-	return []int{}
+func getNeighboursPos(x, y int) []PosOffSet {
+
+	offsets := generateOffsets()
+
+	coordsOffset := []PosOffSet{}
+
+	for _, offset := range offsets {
+		if (x+offset.posX >= 0 && x+offset.posX < MATRIZ_SIZE) && ((y+offset.posY) >= 0 && (y+offset.posY) < MATRIZ_SIZE) {
+
+			coordsOffset = append(coordsOffset, PosOffSet{posX: x + offset.posX, posY: y + offset.posY})
+
+		}
+	}
+
+	return coordsOffset
+}
+
+func verifyNearPos(x, y int, matriz *[][]string) bool {
+	list := getNeighboursPos(x, y)
+
+	// fmt.Println(list)
+
+	for _, v := range list {
+		stringToRune := []rune((*matriz)[v.posX][v.posY])[0]
+		if (*matriz)[v.posX][v.posY] != "." && !unicode.IsDigit(stringToRune) {
+			return true
+		}
+	}
+	return false
+}
+
+// Fazer com regex em cada linha, pega os numeros e depois usa strings.contains para pegar os index, iterava ao redor usando as funcoes de vizinhos :x
+
+type Digit struct {
+	number   int
+	posStart []int
+	posEnd   []int
 }
 
 func main() {
-	// matriz := read("input")
 
-	// getNeighboursPos()
-	fmt.Println(len(generateOffsets()), generateOffsets())
+	matriz := read("input")
+
+	// file, _ := os.ReadFile("input")
+
+	// lines := strings.Split(string(file), "\n")
+
+	// re := regexp.MustCompile(`(\d+)`)
+
+	// allcases := [][]string{}
+
+	// acc := 0
+
+	// for _, v := range lines {
+
+	// 	matches := re.FindAllString(v, -1)
+
+	// 	acc += len(matches)
+
+	// 	allcases = append(allcases, matches)
+	// }
+
+	// fmt.Println(allcases, acc)
 
 	// // Compile the regex pattern
 	// re := regexp.MustCompile(`(\d+)`)
 
 	// // Find all matches in the input string
-	// matches := re.FindAllString(inputString, -1)
+	// matches := re.FindAllString(w1, -1)
 
 	// // Print the matches
 	// fmt.Println(matches)
 
+	// getNeighboursPos()
+
+	// maperson := map[string]int{}
+
+	// listOfNumbers := []string{}
+	// validNumbers := []string{}
+
+	// if verifyNearPos(0, 25, &matriz) {
+	// 	fmt.Println("yay", matriz[0][25])
+	// }
+
+	digit := ""
+	valid := false
+	mapeamento := map[string]bool{}
+	acc := 0
+	for i := 0; i < MATRIZ_SIZE; i++ {
+		for j := 0; j < MATRIZ_SIZE; j++ {
+			x := matriz[i][j]
+			yayFodase := "."
+			if j > 0 {
+				yayFodase = matriz[i][j-1]
+			}
+			anterior := []rune(yayFodase)[0]
+			y := []rune(x)[0]
+			if unicode.IsDigit(y) {
+				digit += x
+
+				if verifyNearPos(i, j, &matriz) {
+					valid = true
+				}
+
+			} else if unicode.IsDigit(anterior) {
+				mapeamento[digit] = valid
+				valid = false
+				digit = ""
+			}
+		}
+		for k, v := range mapeamento {
+			if v {
+				number, _ := strconv.Atoi(k)
+				acc += number
+			}
+		}
+		mapeamento = map[string]bool{}
+	}
+
+	fmt.Println(acc)
+
 }
+
+// 525394
+// 446 + 524948 + 941
+// 2268225
