ece57c11b782279fbd5def7f92b080f963a185ac
Victor Bernardes <victor_luizbernardes5@outlook.com>
2023-12-10 21:52:07 -0300

    day4-part2 missing update

---
+++ day4/go.mod
@@ -0,0 +1,3 @@
+module day4-part2
+
+go 1.21.3
--- day4/main.go
+++ day4/main.go
@@ -4,19 +4,19 @@ import (
 	"fmt"
 	"os"
 	"regexp"
+	"strconv"
 	"strings"
 )
 
-func main() {
+func parseData() map[int]int {
 	file, _ := os.ReadFile("input.txt")
 
 	lines := strings.Split(string(file), "\n")
 
 	var matches1, matches2 []string
 
-	data := map[string]int{}
+	data := map[int]int{}
 
-	allAcc := 0
 	for _, v := range lines {
 		sepCards := strings.Split(v, "|")
 
@@ -34,15 +34,76 @@ func main() {
 		for _, v := range matches1[1:] {
 			for _, k := range matches2 {
 				if v == k {
-					cardAcc = cardAcc * 2
-				}
-				if v == k && cardAcc == 0 {
-					cardAcc = 1
+					cardAcc++
 				}
 			}
 		}
-		allAcc += cardAcc
-		data[cardNumber] = cardAcc
+
+		cardNumberInt, _ := strconv.Atoi(cardNumber)
+
+		data[cardNumberInt] = cardAcc
+
 	}
-	fmt.Println(data, allAcc)
+	return data
+}
+
+type Card struct {
+	number, matches int
+}
+
+// func getMatches(ca []Card) int {
+// 	if len(ca) == 0 {
+// 		return 1
+// 	}
+// 	return 1 + getMatches(ca[])
+// }
+
+/*
+
+{
+	1: 4
+	2: 2
+	3: 1
+	4: 1
+	5: 0
+	6: 0
+}
+
+
+card 1 : [2,3,4,5]
+
+
+*/
+
+func checkCards(data *map[int]int, card int, acc int) int {
+	if v, _ := (*data)[card]; v == 0 {
+		return 1
+	}
+	return checkCards(data, card+1, acc)
+}
+
+// map[1:4 2:2 3:2 4:1 5:0 6:0]
+
+// map[1:1 2:2 3:3 4:1 5:1]
+
+func main() {
+
+	data := parseData()
+
+	anotherData := map[int]int{}
+
+	// cards := []Card
+
+	for k, v := range data {
+		for i := k; i <= v+1; i++ {
+			if v, ok := anotherData[i]; ok {
+				anotherData[i] = v + 1
+			} else {
+				anotherData[i] = 1
+			}
+		}
+	}
+
+	fmt.Println(data, anotherData)
+
 }
