11f6a2bc3ad954b9cffac6ce02651653749be266
Victor Bernardes <victor_luizbernardes5@outlook.com>
2023-12-14 17:46:19 -0300

    day4-part2 done

--- day4/main.go
+++ day4/main.go
@@ -4,20 +4,23 @@ import (
 	"fmt"
 	"os"
 	"regexp"
-	"strconv"
 	"strings"
 )
 
-func parseData() map[int]int {
+var number_inputs int
+
+func parseData() []int {
 	file, _ := os.ReadFile("input.txt")
 
 	lines := strings.Split(string(file), "\n")
 
 	var matches1, matches2 []string
 
-	data := map[int]int{}
+	//data := map[int]int{}
+	data := []int{}
 
 	for _, v := range lines {
+		number_inputs++
 		sepCards := strings.Split(v, "|")
 
 		regExp, _ := regexp.Compile(`(\d+)`)
@@ -25,9 +28,7 @@ func parseData() map[int]int {
 		matches1 = regExp.FindAllString(sepCards[0], -1)
 		matches2 = regExp.FindAllString(sepCards[1], -1)
 
-		cardNumber := matches1[0]
-
-		//		data[cardNumber] =
+		// cardNumberInt, _ := strconv.Atoi(matches1[0])
 
 		cardAcc := 0
 
@@ -39,71 +40,33 @@ func parseData() map[int]int {
 			}
 		}
 
-		cardNumberInt, _ := strconv.Atoi(cardNumber)
-
-		data[cardNumberInt] = cardAcc
-
+		data = append(data, cardAcc)
 	}
 	return data
 }
-
-type Card struct {
-	number, matches int
-}
-
-// func getMatches(ca []Card) int {
-// 	if len(ca) == 0 {
-// 		return 1
-// 	}
-// 	return 1 + getMatches(ca[])
-// }
-
-/*
-
-{
-	1: 4
-	2: 2
-	3: 1
-	4: 1
-	5: 0
-	6: 0
-}
-
-
-card 1 : [2,3,4,5]
-
-
-*/
-
-func checkCards(data *map[int]int, card int, acc int) int {
-	if v, _ := (*data)[card]; v == 0 {
-		return 1
-	}
-	return checkCards(data, card+1, acc)
-}
-
-// map[1:4 2:2 3:2 4:1 5:0 6:0]
-
-// map[1:1 2:2 3:3 4:1 5:1]
 
 func main() {
 
 	data := parseData()
 
-	anotherData := map[int]int{}
+	copies := make([]int, number_inputs)
 
-	// cards := []Card
+	for i := range copies {
+		copies[i] = 1
+	}
 
-	for k, v := range data {
-		for i := k; i <= v+1; i++ {
-			if v, ok := anotherData[i]; ok {
-				anotherData[i] = v + 1
-			} else {
-				anotherData[i] = 1
-			}
+	fmt.Println(copies, data)
+
+	for idx, matches := range data {
+		for j := 1; j < matches+1; j++ {
+			copies[idx+j] += copies[idx]
 		}
 	}
 
-	fmt.Println(data, anotherData)
-
+	acc := 0
+	for _, v := range copies {
+		acc += v
+	}
+	// Sdds de um reduce agora nessas horas :sadge:
+	fmt.Println(acc)
 }
